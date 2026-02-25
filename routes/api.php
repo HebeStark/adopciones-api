55eeb785ad18fed205aa93919bf449e2fee6226b
Hebe Stark <hebe_s@hotmail.com>
2026-02-25 12:18:01 +0100

    Eliminado  el middleware de autenticación y las rutas temporales para centrarse en la capa de dominio

--- routes/api.php
+++ routes/api.php
@@ -1,33 +1,10 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AnimalController;
-//temporal
-use Illuminate\Http\Request;
 
-Route::prefix('v1')->group(function() {
+Route::prefix('v1')->group(function () {
 
-    Route::middleware('auth')->group(function (){
+    Route::get('/animals', [AnimalController::class, 'index']);
 
-        Route::get('/auth-check', function () {
-            return response()->json([
-            'success' => true,
-            'message' => 'Authenticated'
-        ]);
-    
-    });
-
-  Route::get('/animals', [AnimalController::class, 'index']);
-
-  });
-
-});
-
-//Temporal
-Route::middleware('auth')->get('/auth-test', function (Request $request) {
-    return response()->json([
-        'success' => true,
-        'data' => [
-            'user' => $request->user(),
-        ],
-    ]);
 });
