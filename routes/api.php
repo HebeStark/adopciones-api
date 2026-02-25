65915e503917eb1ef98eacc05802b5215843aef5
Hebe Stark <hebe_s@hotmail.com>
2026-02-25 18:18:12 +0100

    refactor(animales): migrar rutas a apiResource bajo el prefijo v1

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,5 @@ use App\Http\Controllers\Api\V1\AnimalController;
 
 Route::prefix('v1')->group(function () {
 
-    Route::get('/animals', [AnimalController::class, 'index']);
-
-    Route::get('/animals/{animal}', [AnimalController::class, 'show']);
-
-    Route::post('/animals', [AnimalController::class, 'store']);
-
-     Route::put('animals/{animal}', [AnimalController::class, 'update']);
-
-     Route::delete('animals/{animal}', [AnimalController::class, 'destroy']);
-
+    Route::apiResource('animals', AnimalController::class);
 });
