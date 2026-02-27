8c2037ba081f99e86c83bb9ea96d994dd58bb89d
Hebe Stark <hebe_s@hotmail.com>
2026-02-27 12:44:32 +0100

    Agregue AuthResource e integre UserResource en el endpoint de registro

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Services\Auth\RegisterService;
-Use App\Http\Requests\Auth\RegisterRequest;
-
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\AuthResource;
 class AuthController extends Controller
 {
    public function __construct(
@@ -21,7 +21,7 @@ class AuthController extends Controller
 
         return response()->json([
             'success' => true,
-            'data' => $result,
+            'data' => new AuthResource($result),
         ], 201);
     }
 }
