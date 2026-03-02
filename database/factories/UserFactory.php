aadbfa54621fc03ffc30e61d9e7d5833372b50e0
Hebe Stark <hebe_s@hotmail.com>
2026-03-02 10:44:38 +0100

    store endpoint con validación, autorización de roles y cobertura de pruebas.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,8 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use App\Models\User;
+use App\Enums\UserRole;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -43,5 +44,19 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function admin(): static
+    {
+    return $this->state(fn () => [
+        'role' => UserRole::ADMIN,
+    ]);
+    }
+
+     public function adopter(): static
+    {
+    return $this->state(fn () => [
+        'role' => UserRole::ADOPTER,
+    ]);
     }
 }
